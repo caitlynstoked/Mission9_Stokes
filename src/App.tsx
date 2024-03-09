125287592f759296ab7693069b9e7280270527f2
caitlynstoked <119529327+caitlynstoked@users.noreply.github.com>
2024-03-08 23:23:32 -0700

    Mission9_Stokes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import teamData from './CollegeBasketballTeams.json';
+interface BBProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
 
+function Welcome() {
+  return <h1>Introducing March Madness 2024!!!</h1>;
+}
+
+class CollegeBasketball extends React.Component<BBProps> {
+  render() {
+    const oneBBTeam = this.props;
+
+    return (
+      <div className="team-box">
+        <h2>School Name:{oneBBTeam.school}</h2>
+        <h3>Mascot: {oneBBTeam.name}</h3>
+        <h3>City: {oneBBTeam.city}</h3>
+        <h3>State: {oneBBTeam.state}</h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div className="team-list">
+      {teamData.teams.map((oneBBTeam) => (
+        <CollegeBasketball {...oneBBTeam} />
+      ))}
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
